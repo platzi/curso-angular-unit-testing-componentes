ee69fbdf034f4d517291b4c1b99a4bc9895514ff
Nicolas Molina Monroy <hi@nicobytes.com>
2022-02-23 13:27:40 -0400

    :construction: Work

--- src/app/pipes/reserve.pipe.spec.ts
+++ src/app/pipes/reserve.pipe.spec.ts
@@ -1,3 +1,8 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { ReservePipe } from './reserve.pipe';
 
 fdescribe('ReservePipe', () => {
@@ -17,4 +22,60 @@ fdescribe('ReservePipe', () => {
     const rta = pipe.transform("123");
     expect(rta).toEqual("321");
   })
+});
+
+
+@Component({
+  template: `
+    <h5>{{ 'amor' | reserve }}</h5>
+    <input [(ngModel)]="text">
+    <p>{{ text | reserve }}</p>
+  `
+})
+class HostComponent {
+  text = '';
+}
+
+fdescribe('ReservePipe from HostComponent', () => {
+
+  let component: HostComponent;
+  let fixture: ComponentFixture<HostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HostComponent, ReservePipe ],
+      imports: [ FormsModule ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should the h5 be "roma"', () => {
+    const h5De = fixture.debugElement.query(By.css('h5'));
+    expect(h5De.nativeElement.textContent).toEqual('roma');
+  });
+
+  it('should apply reverse pipe when typing in the input', () => {
+    const inputDe = fixture.debugElement.query(By.css('input'));
+    const inputEl: HTMLInputElement = inputDe.nativeElement;
+    const pDe = fixture.debugElement.query(By.css('p'));
+
+    expect(pDe.nativeElement.textContent).toEqual('');
+
+    inputEl.value = 'ANA 2'; // 2 ANA
+    inputEl.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(pDe.nativeElement.textContent).toEqual('2 ANA');
+  });
+
 });
